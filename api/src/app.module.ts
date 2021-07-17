450a4a2ced05e7c7de179bd1911d8d6b3a18ea08
cchudant <cchudant@student.42.fr>
2021-07-17 16:04:31 +0200

    api: import UserModule

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -3,12 +3,14 @@ import { Module } from '@nestjs/common'
 import { GraphQLModule } from '@nestjs/graphql'
 import { configService } from './config/config.service'
 import { UserEntity } from './entities/user.entity'
+import { UserModule } from './user/user.module'
 
 @Module({
   imports: [
     MikroOrmModule.forRoot(configService.getMikroOrmConfig()),
     GraphQLModule.forRoot(configService.getGraphQLConfig()),
     MikroOrmModule.forFeature([UserEntity]),
+    UserModule,
   ],
   controllers: [],
   providers: [],
